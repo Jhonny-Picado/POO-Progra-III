84fd22ea0d2778633daac125bd4af1ce1c2ce516
Jhonny Picado Vega <jhonnypv14@estudiantec.cr>
2020-12-23 22:03:29 -0600

    Falta mover pacman

--- Proyecto/src/Vista/Pantalla.java
+++ Proyecto/src/Vista/Pantalla.java
@@ -18,6 +18,7 @@ import javax.swing.JFrame;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -27,12 +28,13 @@ import org.json.simple.parser.ParseException;
  * @author Jhonny Picado
  */
 public class Pantalla extends JFrame implements Observer{
-    public static JLabel Casillas[][]=new JLabel[50][50];
+    private static JLabel Casillas[][]=new JLabel[50][50];
     private static Servidor server;
+    private static int bandera; 
     
     //Constructor de la clase pantalla
     public Pantalla(){
-        //server= new Servidor(5005);
+        bandera=0;
         this.setSize(720, 680);
         JPanel b= new JPanel();
         b.setBackground(Color.BLACK);
@@ -41,20 +43,20 @@ public class Pantalla extends JFrame implements Observer{
         this.setVisible(true);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);  
-       server = new Servidor(5005);
+        server = new Servidor(5005);
         server.addObserver(this);
     }
     
     
     //Metodo que genera la matriz del programa
-    public void generarMatriz(JPanel interfaz) {
+    private void generarMatriz(JPanel interfaz) {
         
         for(int i=0; i < Casillas.length; i++) {
             
             for(int j=0; j < Casillas[i].length; j++) {
                 JLabel btnNuevo = new JLabel();
                 btnNuevo.setOpaque(true);
-                btnNuevo.setBackground(Color.BLACK);
+                btnNuevo.setBackground(Color.blue);
                 Casillas[i][j]=btnNuevo;
                 interfaz.add(Casillas[i][j]);
             }
@@ -63,14 +65,44 @@ public class Pantalla extends JFrame implements Observer{
     }
        
     
-    public void ModificarPixeles(String mensaje){
+    private void ModificarPixeles(String mensaje){
         
-        Color Colores[]={red,blue,gray,white,cyan,orange};
-        Casillas[1][1].setBackground(Colores[(int)(Math.random()*5)]);
-        System.out.println("SI LLEGO : "+ mensaje);
+        JSONObject modificaciones = new JSONObject(mensaje);
+        JSONArray sucesos = modificaciones.getJSONArray("Datos");
+
+       for (int i = 0; i<sucesos.length(); i++) {
+            switch (sucesos.getJSONArray(i).getInt(2)) {
+                case 0:
+                    Colorear(sucesos.getJSONArray(i).getInt(0),sucesos.getJSONArray(i).getInt(1),blue);
+                    break;
+                case 1:
+                    Colorear(sucesos.getJSONArray(i).getInt(0),sucesos.getJSONArray(i).getInt(1),red);
+                    break;
+                case 2:
+                    Colorear(sucesos.getJSONArray(i).getInt(0),sucesos.getJSONArray(i).getInt(1),orange);
+                    break;
+                case 3:
+                    Colorear(sucesos.getJSONArray(i).getInt(0),sucesos.getJSONArray(i).getInt(1),yellow);
+                    break;
+                case 4:
+                    Colorear(sucesos.getJSONArray(i).getInt(0),sucesos.getJSONArray(i).getInt(1),black);
+                    break;
+                case 5:
+                    Colorear(sucesos.getJSONArray(i).getInt(0),sucesos.getJSONArray(i).getInt(1),white);
+                    break;
+                default:
+                    break;
+            }
+        }
         
+        //System.out.println("SI LLEGO : "+ mensaje);
     }
 
+    
+    private void Colorear(int i, int j, Color color){
+        Casillas[i][j].setBackground(color);
+    }
+    
     @Override
     public void update(Observable o, Object o1) {
         this.ModificarPixeles(o1.toString());
